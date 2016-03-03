d4ea6a04216d50b5e1c72a95d6e5566f5ac9bd39
Annika Stuke <annika.stuke@aalto.fi>
2016-03-03 18:05:42 +0200

    Update md_potential.m

--- md_potential.m
+++ md_potential.m
@@ -1,16 +1,12 @@
 function [ pot, g ] = md_potential( r )
-%MD_POTENTIAL calculates potential from coordinates
-%   This is now a Coulomb repulsion between particles
-%   and a harmonic external potential.
-%   If two output arguments present, returns the gradient
-%   needed for optimization.
+% calculates potential from coordinates
 
 N=length(r(:,1)); % assuming first index to be particle index
 pot=sum(sum(r.^2)); % this is x^2+y^2 summed over all particles
 a=1000;
 for i=1:N,
    for j=i+1:N,
-       pot=pot+(-1)^i*(-1)^j/norm(r(i,:)-r(j,:))+a/(norm(r(i,:)-r(j,:)))^2; % 1/r potential between particles
+       pot=pot+(-1)^i*(-1)^j/norm(r(i,:)-r(j,:))+a/(norm(r(i,:)-r(j,:)))^2; % Coulomb potential + a/r^2 short repulsive potential
    end
 end
 
